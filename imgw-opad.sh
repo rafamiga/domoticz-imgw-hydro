98d4e996ee933755b9be02c10457c8bd90da1bfb
rafamiga <rafamiga@gmail.com>
2024-03-16 10:52:17 +0100

    Aktualizacja imgw-opad.sh -- dostosowanie do stanu na 2024-03

--- imgw-opad.sh
+++ imgw-opad.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-SRCURL="https://danepubliczne.imgw.pl/api/data/hydro/"
+SRCURL="https://hydro-back.imgw.pl/station/meteo/status?id="
 DOMOURL="http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=%d&svalue=%f"
 
-#         W-wa IMGW Gda P Płn Wro lotn
-#OPAD_IDS=(252200150 254180260 351160424 252200150 252200150)
-OPAD_IDS=(254180260 351160424 252200150 252200150)
-#               v--- counter
+OPAD_IDS=(
+# Gdańsk-Świbno
+354180155
+# Wrocław-Strachowice
+351160424
+# Warszawa-Bielany
+252200150
+# j.w.
+252200150 )
+
+#         v--v--v------- counter
 OPAD_IDX=(59 60 61 62) # domoticz index
-#                  ^--- counter incremental
+#                  ^---- counter incremental
 
 for ST in ${!OPAD_IDS[@]}; do
     ST_ID=${OPAD_IDS[${ST}]}
     TMP=$(mktemp)
 
-    wget -q "https://hydro.imgw.pl/api/station/meteo/?id=${ST_ID}" -O ${TMP}
-    STAN=$(jq -r -c '.hourlyPrecipRecords | sort_by(.date) | .[-1] | .value' ${TMP}) 
+# stara wersja sprzed 2024-03
+#    STAN=$(jq -r -c '.hourlyPrecipRecords | sort_by(.date) | .[-1] | .value' ${TMP}) 
 #    echo $(printf "${DOMOURL}" ${OPAD_IDX[S]} ${STAN})
-    curl -s $(printf "${DOMOURL}" ${OPAD_IDX[ST]} ${STAN}) > /dev/null
+
+    wget -q "${SRCURL}${ST_ID}" -O ${TMP}
+
+#    cat ${TMP} | jq
+    STAN=$(cat ${TMP} | jq '.status.precip.value')
+#    curl -s $(printf "${DOMOURL}" ${OPAD_IDX[ST]} ${STAN}) > /dev/null
+
     rm "${TMP}"
 done
 
