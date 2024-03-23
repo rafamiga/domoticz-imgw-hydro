b7f654ecf48f2e17b8f15c5a87bf68b8f3b84e2d
rafamiga <rafamiga@gmail.com>
2024-03-23 10:52:10 +0100

    Aktualizacja imgw-opad.sh -- reorganizacja konfiguracji, tablica z parami wartości

--- imgw-opad.sh
+++ imgw-opad.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
 SRCURL="https://hydro-back.imgw.pl/station/meteo/status?id="
-DOMOURL="http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=%d&svalue=%f"
+DOMOURL="http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=%d&svalue=%.2f"
+OUTFORMAT="%.2f"
+#DEBUG="true"
+DEBUG="false"
 
+# nazwa  domoticz idx
 OPAD_IDS=(
 # Gdańsk-Świbno
-354180155
+354180155 59
 # Wrocław-Strachowice
-351160424
+351160424 60
 # Warszawa-Bielany
-252200150
-# j.w.
-252200150 )
+252200150 62
+)
+## j.w.
+#252200150 61 )
 
-#         v--v--v------- counter
-OPAD_IDX=(59 60 61 62) # domoticz index
-#                  ^---- counter incremental
+AIDX=0
+ALEN=${#OPAD_IDS[*]}
 
-for ST in ${!OPAD_IDS[@]}; do
-    ST_ID=${OPAD_IDS[${ST}]}
+while [ ${AIDX} -lt ${ALEN} ]; do
+    ST_ID=${OPAD_IDS[${AIDX}]}
+    AIDX=$((AIDX+1))
+    DOMOIDX=${OPAD_IDS[${AIDX}]}
+    AIDX=$((AIDX+1))
+    eval "${DEBUG}" && echo "ST_ID=${ST_ID} Domoticz IDX=${DOMOIDX}"
+
     TMP=$(mktemp)
-
 # stara wersja sprzed 2024-03
 #    STAN=$(jq -r -c '.hourlyPrecipRecords | sort_by(.date) | .[-1] | .value' ${TMP}) 
 #    echo $(printf "${DOMOURL}" ${OPAD_IDX[S]} ${STAN})
@@ -28,8 +36,12 @@ for ST in ${!OPAD_IDS[@]}; do
     wget -q "${SRCURL}${ST_ID}" -O ${TMP}
 
 #    cat ${TMP} | jq
+#    STAN=$(cat ${TMP} | jq '10 * .status.precip.value') # ustawić dzielnik 10
     STAN=$(cat ${TMP} | jq '.status.precip.value')
-    curl -s $(printf "${DOMOURL}" ${OPAD_IDX[ST]} ${STAN}) > /dev/null
+
+    PAYLOAD=$(printf "${DOMOURL}" ${DOMOIDX} ${STAN})
+    eval "${DEBUG}" && echo "${PAYLOAD}"
+    curl -s "${PAYLOAD}" > /dev/null
 
     rm "${TMP}"
 done
